38969180b6a41d18e9806869015e3062ab3a058d
Ajjupatel066 <ajithpatel066@gmail.com>
2023-09-06 18:00:53 +0000

    error fixed at loader import

--- src/components/ProductItemDetails/index.js
+++ src/components/ProductItemDetails/index.js
@@ -2,7 +2,7 @@
 import {Component} from 'react'
 import {Link} from 'react-router-dom'
 import Cookies from 'js-cookie'
-import {Loader} from 'react-loader-spinner'
+import Loader from 'react-loader-spinner'
 import {BsPlusSquare, BsDashSquare} from 'react-icons/bs'
 
 import Header from '../Header'
